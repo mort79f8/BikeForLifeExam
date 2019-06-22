928cf48e17e5bc6217562de7d0488aae61d25710
Daniel Valsby-Koch <dava@aspit.dk>
2019-06-22 23:41:21 +0200

    Made a header and navigation

---
+++ BikeForLife.Web/Pages/Routes.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BikeForLife.Web.Pages
+{
+    public class RoutesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
