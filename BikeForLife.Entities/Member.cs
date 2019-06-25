d8cd99794750061021ef27a0f857be5d38a47fb3
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-06-25 13:13:27 +0200

    Added a commen to Routes need to be able to get Difficulty for the member to add a ride.

--- BikeForLife.Entities/Member.cs
+++ BikeForLife.Entities/Member.cs
@@ -7,7 +7,7 @@ namespace BikeForLife.Entities
 {
     public class Member
     {
-        private List<Ride> rides;
+        private List<Ride> rides = new List<Ride>();
 
         public int Id { get; set; }
         [Display(Name="Navn")]
@@ -15,12 +15,49 @@ namespace BikeForLife.Entities
         [Display(Name = "Tilmelding Dato")]
         public DateTime EnrollmentDate { get; set; }
         [Display(Name = "Køreniveu")]
-        public Difficulty RideLevel { get; }
+        public Difficulty RideLevel { get
+            {
+                if (rides.Count >= 5)
+                {
+                    return Difficulty.Normal;
+                }
+                else if (rides.Count >= 12 && CheckNumberOfNormalDifficulty() >= 5)
+                {
+                    return Difficulty.Hard;
+                }
+                else if (Rides.Count >= 30 && CheckNumberOfHardDifficulty() >= 10 && BeenAYear())
+                {
+                    return Difficulty.Expert;
+                }
+                else
+                {
+                    return Difficulty.Easy;
+                }
+            }
+        }
+
+        private int CheckNumberOfHardDifficulty()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool BeenAYear()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int CheckNumberOfNormalDifficulty()
+        {
+            throw new NotImplementedException();
+        }
+
         public IReadOnlyList<Ride> Rides { get => rides; }
 
         public bool Add(Ride ride)
         {
-            throw new NotImplementedException();
+               
         }
+
+
     }
 }
