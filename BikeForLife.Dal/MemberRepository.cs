9ec8fe2a900838d780bb45f8cf7e96adff4cb455
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-06-25 10:18:48 +0200

    started on the members files so i can display the members on the members page.

---
+++ BikeForLife.Dal/MemberRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeForLife.Dal
+{
+    class MemberRepository
+    {
+        public List<BikeRoute> GetAll()
+        {
+            string sql = "SELECT * FROM BikeRoutes";
+            DataTable dataTable = ExecuteQuery(sql);
+            if (dataTable == null)
+                throw new InvalidOperationException($"DataTable was null. SQL string is: {sql}");
+            return HandleData(dataTable);
+        }
+
+        private List<BikeRoute> HandleData(DataTable dataTable)
+        {
+            if (dataTable == null)
+                return null;
+
+            List<BikeRoute> routes = new List<BikeRoute>();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                routes.Add(new BikeRoute()
+                {
+                    Id = (int)row["BikeRouteId"],
+                    Name = (string)row["Name"],
+                    Difficulty = (Difficulty)row["Difficulty"],
+                    Length = (double)row["Length"],
+                    Country = (string)row["Country"],
+                    City = (string)row["City"]
+                });
+            }
+            return routes;
+        }
+    }
+}
