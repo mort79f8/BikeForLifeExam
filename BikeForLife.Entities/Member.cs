fec9ddc17476adc7fed27a4f293fb23b9a05ed84
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-06-25 13:49:44 +0200

    added function to add a ride to a member it return true if the ride is able to be added, otherwise it returns false

--- BikeForLife.Entities/Member.cs
+++ BikeForLife.Entities/Member.cs
@@ -17,17 +17,17 @@ namespace BikeForLife.Entities
         [Display(Name = "Køreniveu")]
         public Difficulty RideLevel { get
             {
-                if (rides.Count >= 5)
+                if (rides.Count >= 30 && CheckNumberOfHardDifficulty() >= 10 && BeenAYear())
                 {
-                    return Difficulty.Normal;
+                    return Difficulty.Expert;
                 }
                 else if (rides.Count >= 12 && CheckNumberOfNormalDifficulty() >= 5)
                 {
                     return Difficulty.Hard;
                 }
-                else if (Rides.Count >= 30 && CheckNumberOfHardDifficulty() >= 10 && BeenAYear())
+                else if (rides.Count >= 5)
                 {
-                    return Difficulty.Expert;
+                    return Difficulty.Normal;
                 }
                 else
                 {
@@ -35,27 +35,56 @@ namespace BikeForLife.Entities
                 }
             }
         }
+        public IReadOnlyList<Ride> Rides { get => rides.AsReadOnly(); }
 
         private int CheckNumberOfHardDifficulty()
         {
-            throw new NotImplementedException();
+            int numberOfHard = 0;
+            foreach (var ride in rides)
+            {
+                if (ride.Route.Difficulty == Difficulty.Hard)
+                {
+                    numberOfHard++;
+                }
+            }
+            return numberOfHard;
         }
-
-        private bool BeenAYear()
-        {
-            throw new NotImplementedException();
-        }
-
         private int CheckNumberOfNormalDifficulty()
         {
-            throw new NotImplementedException();
+            int numberOfNormal = 0;
+            foreach (var ride in rides)
+            {
+                if (ride.Route.Difficulty == Difficulty.Normal)
+                {
+                    numberOfNormal++;
+                }
+            }
+            return numberOfNormal;
         }
-
-        public IReadOnlyList<Ride> Rides { get => rides; }
+        private bool BeenAYear()
+        {
+            TimeSpan time = DateTime.Today - EnrollmentDate.Date;
+            if (time.Days >= 365)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
         public bool Add(Ride ride)
         {
-               
+            if (ride.Route.Difficulty <= RideLevel)
+            {
+                rides.Add(ride);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
 
