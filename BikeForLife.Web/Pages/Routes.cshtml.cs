5b3920fa5b9ec65ac936a7c8ef2ac673fe92b571
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-06-25 11:49:48 +0200

    Finished the weather info on the Routes page

--- BikeForLife.Web/Pages/Routes.cshtml.cs
+++ BikeForLife.Web/Pages/Routes.cshtml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using BikeForLife.Dal;
 using BikeForLife.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sommerland.Service;
 
 namespace BikeForLife.Web.Pages
 {
@@ -14,6 +16,7 @@ namespace BikeForLife.Web.Pages
         [BindProperty]
         public BikeRoute BikeRoute { get; set; }
         public List<BikeRoute> BikeRoutes { get; set; } = new List<BikeRoute>();
+        //public OpenWeatherMap weatherMap { get; set; } = new OpenWeatherMap();
         public IActionResult OnGet()
         {
             BikeRouteRepository bikeRouteRepository = new BikeRouteRepository();
@@ -31,6 +34,40 @@ namespace BikeForLife.Web.Pages
         public void OnPost()
         {
 
+        }
+
+        public string GetTempatureBasedOnCity(string city)
+        {
+            OpenWeatherMap weatherMap = new OpenWeatherMap();
+            weatherMap.AppId = "3654de113ecd4a2bf4e4144d9403491b";
+            weatherMap.City = city;
+            double temp = 0.0;
+            try
+            {
+                temp = weatherMap.GetTempature();
+            }
+            catch (WebException)
+            {
+                return "N/A";
+            }
+            return $"{temp}°";
+        }
+
+        public string GetWeatherIconBasedOnCity(string city)
+        {
+            OpenWeatherMap weatherMap = new OpenWeatherMap();
+            weatherMap.AppId = "3654de113ecd4a2bf4e4144d9403491b";
+            weatherMap.City = city;
+            string icon;
+            try
+            {
+                icon = weatherMap.GetWeatherIcon();
+            }
+            catch (WebException)
+            {
+                return "N/A";
+            }
+            return $"{icon}";
         }
     }
 }
