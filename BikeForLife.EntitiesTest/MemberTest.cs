0ede117b613791128d984ba60ed2d65d330f6e2f
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-06-26 09:28:57 +0200

    All Xunit Test described in assigment for Member done

--- BikeForLife.EntitiesTest/MemberTest.cs
+++ BikeForLife.EntitiesTest/MemberTest.cs
@@ -160,7 +160,7 @@ namespace BikeForLife.EntitiesTest
         }
 
         [Fact]
-        public void GetRideLevel_ReturnsHardWith30Rides9IsNormal()
+        public void GetRideLevel_ReturnsHardWith30Rides9IsHard()
         {
             Member member = new Member();
             BikeRoute route1 = new BikeRoute();
@@ -194,6 +194,118 @@ namespace BikeForLife.EntitiesTest
 
             //Assert
             Assert.Equal(expectedDifficulty, actualDiffculty);
+        }
+
+        [Fact]
+        public void GetRideLevel_ReturnsHardWith30Rides10IsHardLessThan1Year()
+        {
+            Member member = new Member();
+            member.EnrollmentDate = DateTime.Parse("2019-05-20");
+            BikeRoute route1 = new BikeRoute();
+            BikeRoute route2 = new BikeRoute();
+            BikeRoute route3 = new BikeRoute();
+            route1.Difficulty = Difficulty.Easy;
+            route2.Difficulty = Difficulty.Normal;
+            route3.Difficulty = Difficulty.Hard;
+            for (int i = 0; i < 7; i++)
+            {
+                Ride ride = new Ride();
+                ride.Route = route1;
+                member.Add(ride);
+            }
+            for (int i = 0; i < 13; i++)
+            {
+                Ride ride = new Ride();
+                ride.Route = route2;
+                member.Add(ride);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Ride ride = new Ride();
+                ride.Route = route3;
+                member.Add(ride);
+            }
+
+            Difficulty expectedDifficulty = Difficulty.Hard;
+
+            //Act
+            Difficulty actualDiffculty = member.RideLevel;
+
+            //Assert
+            Assert.Equal(expectedDifficulty, actualDiffculty);
+        }
+
+        [Fact]
+        public void GetRideLevel_ReturnsExpertWith30Rides()
+        {
+            Member member = new Member();
+            member.EnrollmentDate = DateTime.Parse("2017-05-20");
+            BikeRoute route1 = new BikeRoute();
+            BikeRoute route2 = new BikeRoute();
+            BikeRoute route3 = new BikeRoute();
+            route1.Difficulty = Difficulty.Easy;
+            route2.Difficulty = Difficulty.Normal;
+            route3.Difficulty = Difficulty.Hard;
+            for (int i = 0; i < 7; i++)
+            {
+                Ride ride = new Ride();
+                ride.Route = route1;
+                member.Add(ride);
+            }
+            for (int i = 0; i < 13; i++)
+            {
+                Ride ride = new Ride();
+                ride.Route = route2;
+                member.Add(ride);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Ride ride = new Ride();
+                ride.Route = route3;
+                member.Add(ride);
+            }
+            
+            Difficulty expectedDifficulty = Difficulty.Expert;
+
+            //Act
+            Difficulty actualDiffculty = member.RideLevel;
+
+            //Assert
+            Assert.Equal(expectedDifficulty, actualDiffculty);
+        }
+
+        [Fact]
+        public void AddRide_ReturnsTrueRideWasAdded()
+        {
+            //Arrange
+            Member member = new Member();
+            BikeRoute bikeRoute = new BikeRoute();
+            bikeRoute.Difficulty = Difficulty.Easy;
+            Ride ride = new Ride();
+            ride.Route = bikeRoute;
+
+
+            //Act
+
+            //Assert
+            Assert.True(member.Add(ride));
+        }
+
+        [Fact]
+        public void AddRide_ReturnsFalseRideWasntAdded()
+        {
+            //Arrange
+            Member member = new Member();
+            BikeRoute bikeRoute = new BikeRoute();
+            bikeRoute.Difficulty = Difficulty.Hard;
+            Ride ride = new Ride();
+            ride.Route = bikeRoute;
+
+
+            //Act
+
+            //Assert
+            Assert.False(member.Add(ride));
         }
     }
 }
